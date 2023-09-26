1bc2ab5eeeb44616d40fe277c02b774905169b9d
unknown <1250943698@qq.com>
2023-09-26 15:01:13 +0800

    二级基础渲染

--- src/apis/category.js
+++ src/apis/category.js
@@ -8,3 +8,22 @@ export function getCategoryAPI (id) {
         }
     })
 }
+
+
+export const getCategoryFilterAPI = (id) => {
+    return request({
+      url:'/category/sub/filter',
+      params:{
+        id
+      }
+    })
+  }
+
+  
+export const getSubCategoryAPI = (data) => {
+  return request({
+    url:'/category/goods/temporary',
+    method:'POST',
+    data
+  })
+}
